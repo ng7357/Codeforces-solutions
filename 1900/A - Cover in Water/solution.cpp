8aa170244c337dedf266d7739f85c8ef3de54513
Narendra Godara <narendragodara007@gmail.com>
2026-08-28 23:25:21 +0530

    Add Cover in Water [A] from Codeforces

---
+++ 1900/A - Cover in Water/solution.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        string s;
+ 
+        cin >> n >> s;
+ 
+        // Agar "..." मौजूद है
+        if (s.find("...") != string::npos) {
+            cout << 2 << endl;
+        }
+        else {
+            int count = 0;
+ 
+            for (char c : s) {
+                if (c == '.') {
+                    count++;
+                }
+            }
+ 
+            cout << count << endl;
+        }
+    }
+ 
+    return 0;
+}
